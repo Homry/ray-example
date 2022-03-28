185c4801548817285de730ef6ec9cc88d642b4c0
Sergey Sorokumov <ssorokumov@gmail.com>
2022-03-28 16:28:50 +0300

    test

--- iv/sac/__init__.py
+++
@@ -1,7 +0,0 @@
-from .sac import SACTrainer, VarSACTrainer
-from .ensembleSAC import EnsembleSAC, VarEnsembleSAC
-from .iv_sac import IV_EnsembleSAC, IV_VarEnsembleSAC, IV_VarSAC
-from .sunrise import SunriseSAC, Sunrise_VarEnsembleSAC
-from .uwac import UWACSAC, UWAC_VarEnsembleSAC
-
-from .main import run_sac
